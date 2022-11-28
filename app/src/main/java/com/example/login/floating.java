c96da7d9c35f8a1b969d49ea3b8a113d44c99370
minjin <bhs3392@naver.com>
2022-11-28 14:36:14 +0900

    종류화면/글쓰기화면

---
+++ app/src/main/java/com/example/login/floating.java
@@ -0,0 +1,17 @@
+package com.example.login;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class floating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.write);
+
+ }
+}
