a9e8d4e128817ffb6e1fe2a3b0e6d3754910de6d
minjin <bhs3392@naver.com>
2022-11-28 14:35:53 +0900

    종류화면/글쓰기화면

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -2,15 +2,28 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+
+            Button eat_button = (Button) findViewById(R.id.eat_button);
+            eat_button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), eat_activity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
+
